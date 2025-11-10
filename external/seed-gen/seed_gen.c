3a7ab133e9fb6607900d77691ae685a1f9546f0c
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2025-11-10 02:10:52 +0000

    errors

--- external/seed-gen/seed_gen.c
+++ external/seed-gen/seed_gen.c
@@ -3,6 +3,7 @@ Copyright (c) 2025 Christmas_Missionary - BSD Zero Clause License
 */
 
 #include "seed_gen.h"
+#include "../custom-errors/custom_errors.h"
 
 #ifdef _WIN32
 
@@ -17,12 +18,14 @@ endif()
 
 // For Windows 10 and above
 sg_error sg_generate_seed(void * dst, size_t size) {
+  CE_ERROR(dst != NULL, "Destination ptr is NULL!");
+  CE_ERROR(size != 0, "Destination size is 0!");
   if (dst == NULL || size == 0) {
     return SG_INVALID_PARAMETERS;
   }
-  // Make this one line?
   NTSTATUS status = BCryptGenRandom(BCRYPT_RNG_ALG_HANDLE, dst, size, 0);
   if (!BCRYPT_SUCCESS(status)) {
+    // CE_FATAL(0, "Not all bytes were generated upon request!");
     return SG_ERROR_DURING_GEN;
   }
   return SG_SUCCESS;
@@ -34,24 +37,31 @@ sg_error sg_generate_seed(void * dst, size_t size) {
   #include <stdio.h>
 
 sg_error sg_generate_seed(void * dst, size_t size) {
+  CE_ERROR(dst != NULL, "Destination ptr is NULL!");
+  CE_ERROR(size != 0, "Destination size is 0!");
   if (dst == NULL || size == 0) {
     return SG_INVALID_PARAMETERS;
   }
   FILE * file = fopen("/dev/urandom", "rb");
   if (file == NULL) {
+    // CE_FATAL(0, "/dev/urandom, your computer's built-in RNG, couldn't be opened for seeding!");
     return SG_CANT_OPEN;
   }
   unsigned long chrs_read = fread(dst, 1, size, file);
   if (chrs_read != size) {
     (void)fclose(file);
+    // CE_FATAL(0, "Not all bytes for a seed were generated upon request!");
     return SG_ERROR_DURING_GEN;
   }
   if (fclose(file)) {
+    // CE_FATAL(0, "/dev/urandom, your computer's built-in RNG, couldn't be closed!");
     return SG_CANT_CLOSE;
   }
   return SG_SUCCESS;
 }
 
 #else
-  #error "Operating System is not Windows nor Unix-based!"
+sg_error sg_generate_seed(void * dst, size_t size) {
+  return SG_CANT_OPEN;
+}
 #endif
