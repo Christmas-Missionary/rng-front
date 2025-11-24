5d42417c25699fedf7f904db3f64a2a9de3e61bf
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2025-11-24 03:20:01 +0000

    compatible with GCC

--- unix.sh
+++ unix.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-warnings="--std=c17 -Weverything -Wno-declaration-after-statement -Wno-unsafe-buffer-usage -Wno-pre-c11-compat -Wno-padded"
+warnings="--std=c17 -Wall -Wextra -Wpedantic"
 
    cd tests \
-&& clang $warnings -O3 -DCE_ASSUME_ERROR -DCE_ASSUME_WARN -DCE_NO_FATAL bench.c -o ../a \
-&& clang $warnings -g -fsanitize=address,undefined basic.c ../src/external/custom-errors/custom_errors.c ../src/external/seed-gen/seed_gen.c -o ../b \
+&& cc $warnings -O3 -DCE_ASSUME_ERROR -DCE_ASSUME_WARN -DCE_NO_FATAL bench.c -o ../a \
+&& cc $warnings -g -fsanitize=address,undefined basic.c ../src/external/custom-errors/custom_errors.c ../src/external/seed-gen/seed_gen.c -o ../b \
 && cd .. \
 && ./a && ./b
