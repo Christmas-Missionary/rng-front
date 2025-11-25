ff49044e657ff8ea04ba114d94a163a5f227ff04
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2025-11-25 22:34:01 +0000

    updated unix.sh

--- unix.sh
+++ unix.sh
@@ -3,7 +3,8 @@
 warnings="--std=c17 -Wall -Wextra -Wpedantic"
 
    cd tests \
-&& cc $warnings -O3 -DCE_ASSUME_ERROR -DCE_ASSUME_WARN -DCE_NO_FATAL bench.c -o ../a \
+&& cc $warnings -O3 -march=native -DCE_ASSUME_ERROR -DCE_ASSUME_WARN -DCE_NO_FATAL bench.c -o ../a \
 && cc $warnings -g -fsanitize=address,undefined basic.c ../src/external/custom-errors/custom_errors.c ../src/external/seed-gen/seed_gen.c -o ../b \
+&& cc $warnings -O3 -march=native -DCE_ASSUME_ERROR -DCE_ASSUME_WARN -DCE_NO_FATAL -DSHISHUA_TARGET=SHISHUA_TARGET_SCALAR bench.c -o ../c \
 && cd .. \
-&& ./a && ./b
+&& ./a && ./b && ./c
